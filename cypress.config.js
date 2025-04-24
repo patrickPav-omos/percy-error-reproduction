56e4a42c977f474f2ab47bcc4045fcd028de3287
Patrick Pavliuchik <patrick.p@omos-media.de>
2025-04-24 15:10:28 +0200

    chore(config): add chromeWebSecurity setting to file

--- cypress.config.js
+++ cypress.config.js
@@ -1,10 +1,11 @@
 const { defineConfig } = require('cypress')
 
 module.exports = defineConfig({
-  video: false,
-  e2e: {
-    setupNodeEvents(on, config) {
-      return require('./cypress/plugins/index.js')(on, config)
-    },
-  },
-})
+	video: false,
+	e2e: {
+		setupNodeEvents(on, config) {
+			return require('./cypress/plugins/index.js')(on, config);
+		},
+		chromeWebSecurity: false,
+	},
+});
